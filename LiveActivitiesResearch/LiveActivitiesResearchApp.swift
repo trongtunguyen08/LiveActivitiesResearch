03b7f6f1b2586b823c65982c08fd1a68889e3aea
TU NGUYEN <trongtunguyen08@icloud.com>
2024-06-22 17:04:03 +0700

    Initial Commit

---
+++ LiveActivitiesResearch/LiveActivitiesResearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivitiesResearchApp.swift
+//  LiveActivitiesResearch
+//
+//  Created by Tu Nguyen on 22/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivitiesResearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
